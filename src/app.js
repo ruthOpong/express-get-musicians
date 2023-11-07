f5587f208fb1e7f1f6155395196bfca31f50668b
ruthOpong <ruthopong@gmail.com>
2023-11-07 15:07:49 +0000

    musicians part3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded());
 
 //TODO: Create a GET /musicians route to return all musicians 
 
@@ -14,7 +16,24 @@ app.get('/musicians/:id', async (request, response) => {
     response.json(music);
 })
 
+app.post('/musicians/:id', async (request, response) => {
+    await Musician.create(request.body);
+    response.send("resource successfully updated");
+})
 
+app.put('/musicians/:id', async (request, response) => {
+    let musicId = request.params.id;
+    let music = await Musician.findByPk(musicId);
+    await music.update(request.body);
+    response.send("resource successfully updated");
+})
+
+app.delete('/musicians/:id', async (request, response) => {
+    let musicId = request.params.id;
+    let music = await Musician.findByPk(musicId);
+    music.destroy();
+    response.send("resource successfully deleted");
+})
 
 
 
